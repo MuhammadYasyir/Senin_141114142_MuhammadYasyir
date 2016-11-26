0331c98b30be930e3472ea40fb22c0a641ab0f74
MuhammadYasyir <yasir.muhammad66@gmail.com>
2016-11-26 11:31:49 +0700

    volleyball

---
+++ Senin_141114142_MuhammadYasyir/volleyball/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace volleyball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnHitung = new System.Windows.Forms.Button();
+            this.TxtHasil = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score A";
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(83, 12);
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(100, 20);
+            this.Txt1.TabIndex = 1;
+            // 
+            // Txt2
+            // 
+            this.Txt2.Location = new System.Drawing.Point(83, 51);
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(100, 20);
+            this.Txt2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score B";
+            // 
+            // BtnHitung
+            // 
+            this.BtnHitung.Location = new System.Drawing.Point(39, 97);
+            this.BtnHitung.Name = "BtnHitung";
+            this.BtnHitung.Size = new System.Drawing.Size(124, 23);
+            this.BtnHitung.TabIndex = 4;
+            this.BtnHitung.Text = "Hitung";
+            this.BtnHitung.UseVisualStyleBackColor = true;
+            this.BtnHitung.Click += new System.EventHandler(this.BtnHitung_Click);
+            // 
+            // TxtHasil
+            // 
+            this.TxtHasil.Location = new System.Drawing.Point(83, 159);
+            this.TxtHasil.Name = "TxtHasil";
+            this.TxtHasil.ReadOnly = true;
+            this.TxtHasil.Size = new System.Drawing.Size(100, 20);
+            this.TxtHasil.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hasil";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 261);
+            this.Controls.Add(this.TxtHasil);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnHitung);
+            this.Controls.Add(this.Txt2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Txt1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "VolleyBall";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txt1;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnHitung;
+        private System.Windows.Forms.TextBox TxtHasil;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Senin_141114142_MuhammadYasyir/volleyball/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volleyball
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHitung_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = check(Convert.ToInt64(Txt1.Text), Convert.ToInt64(Txt2.Text)).ToString();
+        }
+        public static long kombinasi(long n, long r)
+        {
+            long hasil = 1;
+            for (long i = 0; i < (n - r); i++)
+            {
+                hasil = hasil * (n - i) / (i + 1);
+            }
+            return hasil % 1000000007;
+        }
+        public static long pow(long b, long n)
+        {
+            if (n == 0)
+                return 1;
+            if (n == 1)
+                return b;
+            long halfn = pow(b, n / 2);
+            if (n % 2 == 0)
+                return (halfn * halfn) % 1000000007;
+            else
+                return ((halfn * halfn) % 1000000007) * b;
+        }
+        public static long check(long scoreA, long scoreB)
+        {
+            if (scoreA < scoreB)
+                return check(scoreB, scoreA);
+            if (scoreA >= (scoreB + 2) && scoreA == 25)
+                return kombinasi((scoreA + scoreB) - 1, scoreB);
+            else if (scoreA == (scoreB + 2) && scoreA >= 26)
+                return kombinasi(48, 24) * pow(2, (scoreB - 24)) % 1000000007;
+            else
+                return 0;
+        }
+        
+    }
+}
